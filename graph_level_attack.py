7879da8038c62d7d8ec7a8d8305c4cb190cd2039
anonymous-attack <66425549+anonymous-attack@users.noreply.github.com>
2020-06-21 23:51:37 +0800

    update graph_level_attack.py

--- graph_level_attack.py
+++ graph_level_attack.py
@@ -7,16 +7,17 @@ import scipy.sparse as sp
 import scipy.linalg as spl
 import os.path as osp
 from tqdm import tqdm
-from math import ceil
+import networkx as nx
 from sklearn.preprocessing import normalize
 
 import torch
 import torch.nn.functional as F
 from torch_geometric.datasets import TUDataset
 import torch_geometric.transforms as T
-from torch_geometric.data import DenseDataLoader
-from torch_geometric.nn import DenseSAGEConv, dense_diff_pool
+from torch_geometric.data import DenseDataLoader, DataLoader
 
+import models.diffpool as diffpool
+import models.gin as gin
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
@@ -30,7 +31,7 @@ parser.add_argument("--pert-rate", default=0.2, type=float,
                     help='Perturbation rate of edges to be flipped.')
 parser.add_argument("--threshold", default=1e-5, type=float,
                     help='Restart threshold of eigen-solutions.')
-parser.add_argument('--model', type=str, default='diffpool',
+parser.add_argument('--target-model', type=str, default='diffpool',
                     help='The target model to be attacked on [gin, diffpool].')
 parser.add_argument('--epoch', type=int, default=21,
                     help='The number of epochs. Default: 21 for ENZYMES dataset, 3 for PROTEINS dataset.')
@@ -39,160 +40,53 @@ args = parser.parse_args()
 data_name = args.dataset
 prec_flips = args.pert_rate
 threshold = args.threshold
+target_model = args.target_model
 epoch = args.epoch
 max_nodes = 150
 
+if target_model == 'diffpool':
+    class MyFilter(object):
+        def __call__(self, data):
+            return data.num_nodes <= max_nodes
 
-class MyFilter(object):
-    def __call__(self, data):
-        return data.num_nodes <= max_nodes
+    path = osp.join(osp.dirname('.'), 'data', data_name+'_dense')
+    dataset = TUDataset(path, name=data_name, transform=T.ToDense(max_nodes),
+                        pre_filter=MyFilter())
+    dataset = dataset.shuffle()
+    n = (len(dataset) + 9) // 10
+    test_dataset = dataset[:n]
+    val_dataset = dataset[n:2 * n]
+    train_dataset = dataset[2 * n:]
+    test_loader = DenseDataLoader(test_dataset, batch_size=20)
+    val_loader = DenseDataLoader(val_dataset, batch_size=20)
+    train_loader = DenseDataLoader(train_dataset, batch_size=20)
+elif target_model == 'gin':
+    path = osp.join(osp.dirname("."), 'data', data_name)
+    dataset = TUDataset(path, name=data_name).shuffle()
 
-path = osp.join(osp.dirname('.'), 'data', data_name+'_dense')
-dataset = TUDataset(path, name=data_name, transform=T.ToDense(max_nodes),
-                    pre_filter=MyFilter())
-dataset = dataset.shuffle()
-n = (len(dataset) + 9) // 10
-test_dataset = dataset[:n]
-val_dataset = dataset[n:2 * n]
-train_dataset = dataset[2 * n:]
-test_loader = DenseDataLoader(test_dataset, batch_size=20)
-val_loader = DenseDataLoader(val_dataset, batch_size=20)
-train_loader = DenseDataLoader(train_dataset, batch_size=20)
-
-class GNN(torch.nn.Module):
-    def __init__(self, in_channels, hidden_channels, out_channels,
-                 normalize=False, add_loop=False, lin=True):
-        super(GNN, self).__init__()
-
-        self.add_loop = add_loop
-
-        self.conv1 = DenseSAGEConv(in_channels, hidden_channels, normalize)
-        self.bn1 = torch.nn.BatchNorm1d(hidden_channels)
-        self.conv2 = DenseSAGEConv(hidden_channels, hidden_channels, normalize)
-        self.bn2 = torch.nn.BatchNorm1d(hidden_channels)
-        self.conv3 = DenseSAGEConv(hidden_channels, out_channels, normalize)
-        self.bn3 = torch.nn.BatchNorm1d(out_channels)
-
-        if lin is True:
-            self.lin = torch.nn.Linear(2 * hidden_channels + out_channels,
-                                       out_channels)
-        else:
-            self.lin = None
-
-    def bn(self, i, x):
-        batch_size, num_nodes, num_channels = x.size()
-
-        x = x.view(-1, num_channels)
-        x = getattr(self, 'bn{}'.format(i))(x)
-        x = x.view(batch_size, num_nodes, num_channels)
-        return x
-
-    def forward(self, x, adj, mask=None):
-        #batch_size, num_nodes, in_channels = x.size()
-
-        x0 = x
-        x1 = self.bn(1, F.relu(self.conv1(x0, adj, mask)))
-        x2 = self.bn(2, F.relu(self.conv2(x1, adj, mask)))
-        x3 = self.bn(3, F.relu(self.conv3(x2, adj, mask)))
-
-        x = torch.cat([x1, x2, x3], dim=-1)
-
-        if self.lin is not None:
-            x = F.relu(self.lin(x))
-
-        return x
-
-
-class Net(torch.nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-
-        num_nodes = ceil(0.25 * max_nodes)
-        dim = 3
-        self.gnn1_pool = GNN(3, 64, num_nodes, add_loop=True)
-        self.gnn1_embed = GNN(3, 64, 64, add_loop=True, lin=False)
-
-        num_nodes = ceil(0.25 * num_nodes)
-        self.gnn2_pool = GNN(dim * 64, 64, num_nodes)
-        self.gnn2_embed = GNN(dim * 64, 64, 64, lin=False)
-
-        self.gnn3_embed = GNN(dim * 64, 64, 64, lin=False)
-
-        self.lin1 = torch.nn.Linear(dim * 64, 64)
-        self.lin2 = torch.nn.Linear(64, 6)
-
-    def forward(self, x, adj, mask=None):
-        s = self.gnn1_pool(x, adj, mask)
-        x = self.gnn1_embed(x, adj, mask)
-
-        x, adj, l1, e1 = dense_diff_pool(x, adj, s, mask)
-
-        s = self.gnn2_pool(x, adj)
-        x = self.gnn2_embed(x, adj)
-
-        x, adj, l2, e2 = dense_diff_pool(x, adj, s)
-
-        x = self.gnn3_embed(x, adj)
-
-        x = x.mean(dim=1)
-        x = F.relu(self.lin1(x))
-        x = self.lin2(x)
-        return F.log_softmax(x, dim=-1), l1 + l2, e1 + e2
-
+    test_dataset = dataset[:len(dataset) // 10]
+    train_dataset = dataset[len(dataset) // 10:]
+    test_loader = DataLoader(test_dataset, batch_size=len(test_dataset))
+    train_loader = DataLoader(train_dataset, batch_size=128)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Net().to(device)
+if target_model == 'diffpool':
+    model = diffpool.Net().to(device)
+elif target_model == 'gin':
+    model = gin.Net(dataset.num_features, dataset.num_classes).to(device)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
-def train(epoch):
-    model.train()
-    loss_all = 0
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        if len(data.adj.shape) > 3:
-            data_adj = data.adj[:,:,:,0] + data.adj[:,:,:,1] + data.adj[:,:,:,2] + data.adj[:,:,:,3]
-        else:
-            data_adj = data.adj
-        if data.x == None:
-            bastchsize, n_nodes = data.adj.shape[0], data.adj.shape[1]
-            data_x = torch.eye(n_nodes).reshape((1,n_nodes,n_nodes)).repeat(bastchsize, 1, 1).to(device)
-        else:
-            data_x = data.x
-        output, _, _ = model(data_x, data_adj, data.mask)
-        loss = F.nll_loss(output, data.y.view(-1))
-        loss.backward()
-        loss_all += data.y.size(0) * loss.item()
-        optimizer.step()
-    return loss_all / len(train_dataset)
-
-@torch.no_grad()
-def test(loader):
-    model.eval()
-    correct = 0
-
-    for data in loader:
-        data = data.to(device)
-
-        if len(data.adj.shape) > 3:
-            data_adj = data.adj[:, :, :, 0] + data.adj[:, :, :, 1] + data.adj[:, :, :, 2] + data.adj[:, :, :, 3]
-        else:
-            data_adj = data.adj
-        if data.x == None:
-            bastchsize, n_nodes = data.adj.shape[0], data.adj.shape[1]
-            data_x = torch.eye(n_nodes).reshape((1, n_nodes, n_nodes)).repeat(bastchsize, 1, 1).to(device)
-        else:
-            data_x = data.x
-        pred = model(data_x, data_adj, data.mask)[0].max(dim=1)[1]
-
-        correct += pred.eq(data.y.view(-1)).sum().item()
-    return correct / len(loader.dataset)
-
-for epoch in range(1,21):
-    train_loss = train(epoch)
-    train_acc = test(train_loader)
-    test_acc = test(test_loader)
+for epoch in range(1, 21):
+    if target_model == 'diffpool':
+        train_loss = diffpool.train(epoch, model, optimizer, train_loader, train_dataset, device)
+        train_acc = diffpool.test(train_loader, model, device)
+        test_acc = diffpool.test(test_loader, model, device)
+    elif target_model == 'gin':
+        train_loss = gin.train(epoch, model, optimizer, train_loader, train_dataset, device)
+        train_acc = gin.test(train_loader, model, device)
+        test_acc = gin.test(test_loader, model, device)
     print('Epoch: {:03d}, Train Loss: {:.7f}, '
           'Train Acc: {:.7f}, Test Acc: {:.7f}'.format(epoch, train_loss,
                                                        train_acc, test_acc))
@@ -268,12 +162,18 @@ def eigen_restart_flips(adj_matrix, D_, candidates, n_flips, vals_org, vecs_org,
     return best_edges
 
 def propose_attack(model, g, prec_flips=0.2, threshold=1e-5):
-    if len(g.adj.shape) > 2:
-        A = g.adj[:, :, 0] + g.adj[:, :, 1] + g.adj[:, :, 2] + g.adj[:, :, 3]
-    else:
-        A = g.adj
-    A = A.numpy()
-    _A_obs = sp.csr_matrix(A)
+    if target_model == 'diffpool':
+        if len(g.adj.shape) > 2:
+            A = g.adj[:, :, 0] + g.adj[:, :, 1] + g.adj[:, :, 2] + g.adj[:, :, 3]
+        else:
+            A = g.adj
+        A = A.numpy()
+        _A_obs = sp.csr_matrix(A)
+    elif target_model == 'gin':
+        g_edges = g.edge_index.numpy().T
+        g_nx = nx.Graph()
+        g_nx.add_edges_from(g_edges)
+        _A_obs = nx.adjacency_matrix(g_nx)
 
     _N = _A_obs.shape[0]
     _E = int(np.sum(_A_obs) / 2)
